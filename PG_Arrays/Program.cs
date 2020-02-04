cbdd781f6abc73150ea5b5d5b0e16c8ff7b93a53
Pavlo Grytsenko <pavlo@usf.edu>
2020-02-04 18:10:22 -0500

    push

---
+++ PG_Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PG_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 25;
+            Console.WriteLine("This is an Auto-Populated Array Program ");
+            Console.WriteLine("The chosen number for this program is: 25");
+            Console.WriteLine("Lets begin!");
+            Console.WriteLine("-----------------------------------------");
+            int[] array = new int[number];
+            int populate_array = 0;
+            for (int index = 1; index < number; index++)
+            {
+                array[index] = populate_array;
+                Console.Write("Element value = " + index + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
