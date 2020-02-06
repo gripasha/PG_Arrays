812aff17f33a3678951a8e066dd197625f0c2e32
Pavlo Grytsenko <pavlo@usf.edu>
2020-02-06 18:06:28 -0500

    Completed.

--- PG_Arrays/Program.cs
+++ PG_Arrays/Program.cs
@@ -10,7 +10,7 @@ namespace PG_Arrays
             Console.WriteLine("-------------------------------------------");
             // Declare an array with 25 elements
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
-            foreach (int i in numbers)
+            foreach (int i in numbers) // Using a foreach loop to iterate through an array
             {
                 Console.Write("Element Value = " + i);
                 Console.WriteLine();
