cf9d1959e288c0be8011c260f67638f57918c84e
Pasha-PC <Pasha-PC@DESKTOP-C1APF31>
2020-02-05 19:18:02 -0500

    update

--- PG_Arrays/Program.cs
+++ PG_Arrays/Program.cs
@@ -6,20 +6,22 @@ namespace PG_Arrays
     {
         static void Main(string[] args)
         {
-            int number = 25;
+            // Declaring a number of 25 for the array size
+            int number = 25; 
             Console.WriteLine("This is an Auto-Populated Array Program ");
             Console.WriteLine("The chosen number for this program is: 25");
             Console.WriteLine("Lets begin!");
             Console.WriteLine("-----------------------------------------");
-            int[] array = new int[number];
-            int populate_array = 0;
-            for (int index = 1; index < number; index++)
+            // Declaring an array with the size of 25 
+            // the +1 is needed for the countdown to start from 1 and not 0
+            int[] array = new int[number+1];
+            // Using the for loop to iterate throught the array 
+            for (int index = 1; index < number+1; index++)
             {
-                array[index] = populate_array;
                 Console.Write("Element value = " + index + " ");
                 Console.WriteLine();
             }
-            Console.WriteLine("-----------------------------------------");
-        }
+            Console.WriteLine("-----------------------------------------"); 
+        } // END
     }
 }
